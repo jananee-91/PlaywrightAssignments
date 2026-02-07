f7c73652df0d965d37d2d589f364e9d388e48007
jananee-91 <jananee.r1@gmail.com>
2026-02-06 21:40:15 -0500

    Add files via upload

---
+++ day2/PrintOddNumber.js
@@ -0,0 +1,7 @@
+let num =30
+for(let i=0;i<=num;i++){
+    if(i%2!=0){
+        console.log("Odd Number:" +i)
+    }
+
+}
---
+++ day2/PrintRepeatedChar.js
@@ -0,0 +1,10 @@
+let empname = "jananeeravi"
+let targetChar = 'a'
+let count =0
+for(let i=0;i<empname.length;i++)
+{
+    if(empname.charAt(i)==targetChar){
+       count++
+    }  
+    }
+console.log("The final count of a is: "+ count)
